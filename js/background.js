da2867c79fcbd787f149619a15981712ff7e4b64
Richard Leurs <r1chardtm@users.noreply.github.com>
2018-02-23 21:23:48 +0100

    Remove debugger

--- js/background.js
+++ js/background.js
@@ -1,5 +1,4 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-    debugger;
     if(tab.url.includes("/issues/")){
         chrome.tabs.executeScript(tab.id, {file: "js/content.js"});
     }
